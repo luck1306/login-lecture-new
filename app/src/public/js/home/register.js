fe0a66912eff0c3b5fa6cdc519b487e372ffb339
luckzero1 <luck1306@naver.com>
2022-01-30 02:33:09 +0900

    24-1

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -15,20 +15,20 @@ function register() {
         psword: psword.value,
         confirmPsword: confirmPsword.value,
     };
-
-    fetch("/register", {
-        method:"POST",
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        body:JSON.stringify(req),
-    }).then((res) => res.json())
-        .then((res) => {
-            if(res.success){
-                location.href = "/login";
-            }
-            else {
-                alert(res.msg);
-            }
-        }).catch((err) => console.error(new Error("회원가입 중 에러 발생")));
+    console.log(req);
+    // fetch("/register", {
+    //     method:"POST",
+    //     headers: {
+    //         "Content-Type" : "application/json",
+    //     },
+    //     body:JSON.stringify(req),
+    // }).then((res) => res.json())
+    //     .then((res) => {
+    //         if(res.success){
+    //             location.href = "/login";
+    //         }
+    //         else {
+    //             alert(res.msg);
+    //         }
+    //     }).catch((err) => console.error(new Error("회원가입 중 에러 발생")));
 }
